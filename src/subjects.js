58013b979933c15df241ef38256600ede450e5da
Tejas Mahajan <59790915+Tejas242@users.noreply.github.com>
2023-08-12 14:17:53 +0000

    Update subjects.js

--- src/subjects.js
+++ src/subjects.js
@@ -2,53 +2,53 @@ const subjects = [
   {
     courseCode: 1,
     name: "Engineering Mathematics-II",
-    credits: 4
+    credits: 4,
   },
   {
     courseCode: 2,
     name: "Engineering Physics",
-    credits: 4
+    credits: 4,
   },
   {
     courseCode: 3,
     name: "Engineering Graphics & Design",
-    credits: 2
+    credits: 2,
   },
   {
     courseCode: 4,
     name: "LAB-Engineering Graphics & Design",
-    credits: 1
+    credits: 1,
   },
   {
     courseCode: 5,
     name: "Basics of Electronics Engineering",
-    credits: 2
+    credits: 3,
   },
   {
     courseCode: 6,
     name: "LAB-Basics of Electronics Engineering",
-    credits: 1
+    credits: 1,
   },
   {
     courseCode: 7,
     name: "Communication Skills",
-    credits: 2
+    credits: 2,
   },
   {
     courseCode: 8,
     name: "LAB-Communication Skills",
-    credits: 1
+    credits: 1,
   },
   {
     courseCode: 9,
     name: "LAB-Computer Workshop",
-    credits: 2
+    credits: 2,
   },
   {
     courseCode: 10,
     name: "LAB-Engineering Physics",
-    credits: 1
-  }
+    credits: 1,
+  },
 ];
 
 export default subjects;
