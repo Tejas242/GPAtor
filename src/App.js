143d6423b53de91a4afded737f4858bdf5f718c9
Tejas Mahajan <59790915+Tejas242@users.noreply.github.com>
2023-08-13 05:58:25 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
       <h1>GPAtor</h1>
       <p>Your own GPA Calculator</p>
       <DropdownSelector />
+      <footer>Star on <a href="https://github.com/Tejas242/GPAtor">GitHub</a></footer>
     </div>
   );
 }
